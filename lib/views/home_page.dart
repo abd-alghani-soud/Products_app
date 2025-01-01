db343c8bfdb411c4208fb0c87c4cc81c37467133
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-02 00:26:20 +0300

    just create file home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -5,9 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.black,
-    ));
+    return const Scaffold();
   }
 }
