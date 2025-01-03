9b4f21e21ce479cd2e5d4397d06074530961a8c6
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-04 02:27:44 +0300

    create products page

--- lib/views/products_page.dart
+++ lib/views/products_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:freezed_code/constant.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kColorOr,
+      ),
+    );
   }
 }
