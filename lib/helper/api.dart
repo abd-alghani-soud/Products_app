2fafcb75a2529bf1b99d5ffd947ade07e6f9be17
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-06 02:05:34 +0300

    add post post request

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -12,4 +12,36 @@ class Api {
           'there is a problem with status code ${response.statusCode}');
     }
   }
+
+  Future<dynamic> post(
+      {required String url, dynamic body, String? token}) async {
+    Map<String, String> headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception(
+        "there is a problem with status code ${response.statusCode} and body's ${json.decode(response.body)}",
+      );
+    }
+  }
 }
+/*
+*
+* headers
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer $token',
+ */
