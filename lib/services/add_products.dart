6f993de4885f430916ee9accca48962599c00b63
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-06 02:06:25 +0300

    add add products services

---
+++ lib/services/add_products.dart
@@ -0,0 +1,23 @@
+import 'package:freezed_code/helper/api.dart';
+import 'package:freezed_code/models/products_model.dart';
+
+class AddProducts {
+  Future<ProductsModel> addProducts({
+    required String title,
+    required String image,
+    required String price,
+    required String desc,
+    required String category,
+  }) async {
+    Map<String, dynamic> data =
+        await Api().post(url: 'https://fakestoreapi.com/products', body: {
+      'title': title,
+      'price': price,
+      'description': desc,
+      'image': image,
+      'category': category
+    });
+
+    return ProductsModel.fromJson(data);
+  }
+}
