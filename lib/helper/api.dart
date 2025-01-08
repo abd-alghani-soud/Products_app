f4ff6365214fb907de764b9412f1eb150285f5de
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-08 22:13:37 +0300

    add put request in api

--- lib/helper/api.dart
+++ lib/helper/api.dart
@@ -37,10 +37,35 @@ class Api {
       );
     }
   }
+
+  Future<dynamic> put(
+      {required String url, dynamic body, String? token}) async {
+    Map<String, String> headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    };
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      return data;
+    } else {
+      throw Exception(
+        "there is a problem with status code ${response.statusCode} and body's ${json.decode(response.body)}",
+      );
+    }
+  }
 }
 /*
-*
-* headers
+      headers
       'Accept': 'application/json',
       'Content-Type': 'application/x-www-form-urlencoded',
       'Authorization': 'Bearer $token',
