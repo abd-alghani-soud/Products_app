81ba5a4f27e41f20b1f47803b2e1fcdbae38bcb1
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-09 00:12:19 +0300

    create card in products page

--- lib/views/products_page.dart
+++ lib/views/products_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:freezed_code/constant.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -8,7 +9,83 @@ class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.cartShopping,
+              color: kColorBl,
+              size: 26,
+            ),
+          ),
+        ],
         backgroundColor: kColorOr,
+        centerTitle: true,
+        title: const Text(
+          "New Trend",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            color: kColorBl,
+          ),
+        ),
+        elevation: 0,
+      ),
+      // ignore: sized_box_for_whitespace
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 0,
+              blurRadius: 40,
+              offset: const Offset(10, 10),
+              color: Colors.grey.withOpacity(.2),
+            ),
+          ],
+        ),
+        height: 150,
+        width: 220,
+        child: Card(
+          elevation: 10,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 4,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "bugs",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      r"$200",
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
