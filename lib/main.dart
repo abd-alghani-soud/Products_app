c8f92242eecebdf2187ff0687da1bb2881169acb
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-10 02:33:31 +0300

    edit names of pages

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_code/blocs/auth/auth_bloc.dart';
-import 'package:freezed_code/views/home_page.dart';
+import 'package:freezed_code/views/first_page.dart';
 
 import 'firebase_options.dart';
 
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       create: (BuildContext context) => AuthBloc()..add(CheakAuthStatusEvent()),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: FirstPage(),
       ),
     );
   }
