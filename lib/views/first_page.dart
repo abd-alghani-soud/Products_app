9bc166fb91ca1762109a643360639bba5135d690
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-01-10 02:31:04 +0300

    create some pages to add bottom navigation bar

---
+++ lib/views/first_page.dart
@@ -0,0 +1,117 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:freezed_code/constant.dart';
+import 'package:freezed_code/views/login_page.dart';
+import 'package:freezed_code/views/products_page.dart';
+
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _cartAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(
+        seconds: 2,
+      ),
+      vsync: this,
+    );
+
+    _cartAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: const Offset(-0.5, 0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _controller.forward();
+
+    Timer(
+      const Duration(
+        seconds: 5,
+      ),
+          () {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const ProductsPage(),
+            ),
+          );
+        } else {
+          // User is not signed in, navigate to LoginPage
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LoginPage(),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            'assets/images/hm.jpg',
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SlideTransition(
+              position: _cartAnimation,
+              child: const Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    Icons.shopping_cart,
+                    size: 100,
+                    color: Colors.black,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      "Shop Now",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: kColorOr,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,116 +1,63 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:freezed_code/constant.dart';
-import 'package:freezed_code/views/login_page.dart';
-import 'package:freezed_code/views/products_page.dart';
+import 'package:freezed_code/models/products_model.dart';
+import 'package:freezed_code/services/get_all_products_services.dart';
+import 'package:freezed_code/widgets/custom_card.dart';
 
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _cartAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(
-        seconds: 2,
-      ),
-      vsync: this,
-    );
-
-    _cartAnimation = Tween<Offset>(
-      begin: const Offset(-1, 0),
-      end: const Offset(-0.5, 0),
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
-    );
-    _controller.forward();
-
-    Timer(
-      const Duration(
-        seconds: 5,
-      ),
-          () {
-        final user = FirebaseAuth.instance.currentUser;
-        if (user != null) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const ProductsPage(),
-            ),
-          );
-        } else {
-          // User is not signed in, navigate to LoginPage
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => LoginPage(),
-            ),
-          );
-        }
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            'assets/images/hm.jpg',
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: double.infinity,
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: SlideTransition(
-              position: _cartAnimation,
-              child: const Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(
-                    Icons.shopping_cart,
-                    size: 100,
-                    color: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 16.0,
+          right: 16,
+          top: 60,
+        ),
+        child: FutureBuilder<List<ProductsModel>>(
+          future: GetAllProductsServices().getAllProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              debugPrint('${snapshot.error}');
+              return Center(
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 16,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      "Shop Now",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: kColorOr,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+                ),
+              );
+            } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+              List<ProductsModel> products = snapshot.data!;
+              return GridView.builder(
+                itemCount: products.length,
+                clipBehavior: Clip.none,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 1.4,
+                  mainAxisSpacing: 80,
+                ),
+                itemBuilder: (context, index) {
+                  return CustomCard(
+                    products: products[index],
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text(
+                  'No products available.',
+                  style: TextStyle(fontSize: 18, color: Colors.grey),
+                ),
+              );
+            }
+          },
+        ),
       ),
     );
   }
