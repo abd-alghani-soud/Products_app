f550b03aeb2fa4a7c1bfa95253ba7d20225b42bc
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-02-09 22:54:50 +0300

    create a profile page

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -1,10 +1,115 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:freezed_code/constant.dart';
+import 'package:freezed_code/views/login_page.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    User? user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Profile Page',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+        backgroundColor: kColorOr,
+        elevation: 5,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.4),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                  )
+                ],
+              ),
+              child: const CircleAvatar(
+                radius: 70,
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: 65,
+                  backgroundImage: AssetImage('assets/images/p.jpg'),
+                  backgroundColor: kColorOr,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                  )
+                ],
+              ),
+              child: Text(
+                user?.email ?? 'No User',
+                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500,),
+              ),
+            ),
+            const SizedBox(height: 50),
+            GestureDetector(
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
+              },
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 30),
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: kColorOr,
+                  boxShadow: [
+                    BoxShadow(
+                      color: kColorOr.withOpacity(0.5),
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    'Log Out',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -12,92 +12,86 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final favoritesCubit = context.read<FavoritesCubit>();
-
-    return BlocListener<FavoritesCubit, List<ProductsModel>>(
-      listener: (context, state) {
-
-      },
-      child: GestureDetector(
-        onTap: () {},
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    spreadRadius: 0,
-                    blurRadius: 40,
-                    offset: const Offset(10, 10),
-                    color: Colors.grey.withOpacity(.2),
-                  ),
-                ],
-              ),
-              child: Card(
-                color: Colors.white,
-                elevation: 10,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                    vertical: 4,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        products.title.substring(0, 7),
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                        ),
+    return GestureDetector(
+      onTap: () {},
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  spreadRadius: 0,
+                  blurRadius: 40,
+                  offset: const Offset(10, 10),
+                  color: Colors.grey.withOpacity(.2),
+                ),
+              ],
+            ),
+            child: Card(
+              color: Colors.white,
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 4,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      products.title.substring(0, 7),
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            r'$' '${products.price.toString()}',
-                            style: const TextStyle(
-                              fontSize: 16,
-                            ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          r'$' '${products.price.toString()}',
+                          style: const TextStyle(
+                            fontSize: 16,
                           ),
-                          BlocBuilder<FavoritesCubit, List<ProductsModel>>(
-                            builder: (context, favorites) {
-                              return IconButton(
-                                onPressed: () {
-                                  if (favoritesCubit.isFavorite(products)) {
-                                    favoritesCubit.removeFromFavorites(products);
-                                  } else {
-                                    favoritesCubit.addToFavorites(products);
-                                  }
-                                },
-                                icon: Icon(
-                                  Icons.favorite,
-                                  color: favoritesCubit.isFavorite(products)
-                                      ? Colors.red
-                                      : Colors.grey,
-                                ),
-                              );
-                            },
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
+                        ),
+                        BlocBuilder<FavoritesCubit, List<ProductsModel>>(
+                          builder: (context, favorites) {
+                            return IconButton(
+                              onPressed: () {
+                                if (favoritesCubit.isFavorite(products)) {
+                                  favoritesCubit.removeFromFavorites(products);
+                                } else {
+                                  favoritesCubit.addToFavorites(products);
+                                }
+                              },
+                              icon: Icon(
+                                Icons.favorite,
+                                color: favoritesCubit.isFavorite(products)
+                                    ? Colors.red
+                                    : Colors.grey,
+                              ),
+                            );
+                          },
+                        )
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
-            Positioned(
-              top: -50,
-              right: 32,
-              child: Image.network(
-                products.image,
-                height: 100,
-                width: 100,
-              ),
+          ),
+          Positioned(
+            top: -50,
+            right: 32,
+            child: Image.network(
+              products.image,
+              height: 100,
+              width: 100,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
